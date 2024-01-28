02cf38bf43b2d7370ebd623f6f60fd4195102cce
Evan Joseph <evandromariano49@gmail.com>
2024-01-28 12:32:39 -0300

    feat: cors adicionado

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-
 const app = express();
+const cors = require("cors");
 require("dotenv").config(); // permite a aplicação trabalhar com variáveis de ambiente
 const PORT = process.env.PORT || 3000;
 // documentação da API
@@ -9,6 +9,7 @@ const swagger = require("./configs/swagger.js");
 
 // configs
 app.use(express.json());
+app.use(cors());
 
 // Rota para servir a documentação do Swagger
 app.use("/api-docs", swaggerUi.serve, swagger);
