6b75f2d098665e83163c8daf02d6cb604c9ce4c4
Evan Joseph <evandromariano49@gmail.com>
2024-01-27 21:29:29 -0300

    conexção com banco

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const app = express();
+require("dotenv").config(); // permite a aplicação trabalhar com variáveis de ambiente
+const PORT = process.env.PORT || 3000;
+
+// configs
+app.use(express.json());
+
+// routes
+
+app.get("/", (_, res) => {
+  return res.json("Hello World!");
+});
+
+// Conexão com o banco
+
+const connectDB = require("./db/connectMongo");
+
+connectDB();
+
+// iniciando servidor
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta " + PORT);
+});
