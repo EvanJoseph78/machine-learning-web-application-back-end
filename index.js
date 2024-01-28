3463cd7cf6d32b2ff2bd5db7e36b827c26ffb1a9
Evan Joseph <evandromariano49@gmail.com>
2024-01-28 04:35:55 -0300

    feet: tentando fazer o swagger rodar na vercel

--- index.js
+++ index.js
@@ -15,7 +15,15 @@ app.use(express.json());
 
 // Rota para servir a documentação do Swagger
 // app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+const CSS_URL =
+  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocs, { customCssUrl: CSS_URL }),
+);
 
 // routes
 
