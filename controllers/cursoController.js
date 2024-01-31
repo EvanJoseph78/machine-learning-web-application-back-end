f6929166b533e84170b69907774b6e7a953f6091
Evan Joseph <evandromariano49@gmail.com>
2024-01-31 05:33:04 -0300

    feat: criadas validações

--- controllers/cursoController.js
+++ controllers/cursoController.js
@@ -50,13 +50,25 @@ const cursoController = {
         return res.status(404).json({ error: "Curso não encontrado" });
       }
 
+
       // Extrai os dados do módulo do corpo da solicitação
       const novoModulo = {
         numeromodulo: req.body.numeromodulo,
         titulo: req.body.titulo,
         linkcapa: req.body.linkcapa,
-        aulas: req.body.aulas,
       };
+
+      // Verifica se já existe um módulo com o mesmo número
+      const moduloExistente = curso.modulos.find(modulo => modulo.numeromodulo === novoModulo.numeromodulo);
+
+      if (moduloExistente) {
+        return res.status(400).json({ error: "Já existe um módulo com o mesmo número" });
+      }
+
+      // Garante que numeromodulo é um número inteiro
+      if (!Number.isInteger(novoModulo.numeromodulo) || novoModulo.numeromodulo <= 0) {
+        return res.status(400).json({ error: "O número do módulo deve ser um número inteiro maior que 0" });
+      }
 
       // Adiciona o módulo ao array de módulos do curso
       curso.modulos.push(novoModulo);
@@ -136,6 +148,18 @@ const cursoController = {
         linkcapa: req.body.linkcapa,
         materiaisextras: req.body.materiaisextras,
       };
+
+      // Verifica se numeroaula é um número inteiro
+      if (!Number.isInteger(numeroaula) || numeroaula <= 0) {
+        return res.status(400).json({ error: "O número da aula deve ser um número inteiro maior que 0" });
+      }
+
+      // Verifica se já existe uma aula com o mesmo número
+      const aulaExistente = modulo.aulas.find(aula => aula.numeroaula === novaAula.numeroaula);
+
+      if (aulaExistente) {
+        return res.status(400).json({ error: "Já existe uma aula com o mesmo número" });
+      }
 
       // Adiciona a aula ao array de aulas do módulo
       modulo.aulas.push(novaAula);
