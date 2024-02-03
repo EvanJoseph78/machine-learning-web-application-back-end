68ae79ee06d781de0141cca50c9ddcd59e38fffb
Evan Joseph <evandromariano49@gmail.com>
2024-02-03 03:27:15 -0300

    fix: resposta da rota /cursos

--- controllers/cursoController.js
+++ controllers/cursoController.js
@@ -31,7 +31,8 @@ const cursoController = {
   getAll: async (req, res) => {
     try {
       const cursos = await CursoModel.find();
-      res.json(cursos);
+      const quantidadeCursos = cursos.length;
+      res.json({ cursos: cursos, qntcursos: quantidadeCursos });
     } catch (error) {
       console.error(error);
       res.status(500).json({ msg: "Internal server error" });
