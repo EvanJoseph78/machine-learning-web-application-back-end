2728d9cf9b8097375ca0d8e36c4c80d24bbb8831
evan <evandromariano49@gmail.com>
2024-02-28 23:11:15 -0300

    feat: add cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import modulo from "./routes/module.js";
 import classes from "./routes/classes.js";
 import questions from "./routes/questions.js";
 import cookieParser from "cookie-parser";
-import cors from 'cors';
+import cors from "cors";
 
 //configs
 const app = express();
