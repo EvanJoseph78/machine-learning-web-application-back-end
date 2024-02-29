b3ca49dc032b3de3f3e93824da1b75d8070413b2
evan <evandromariano49@gmail.com>
2024-02-28 23:49:02 -0300

    fix: test4

--- controllers/course.js
+++ controllers/course.js
@@ -2,9 +2,9 @@ import Course from "../models/Course.js";
 
 export const getAllCourses = async (req, res) => {
   try {
-    // const cursos = await Course.find();
-    // const quantidadeCursos = cursos.length;
-    // res.json({ cursos: cursos, qntcursos: quantidadeCursos });
+    const cursos = await Course.find();
+    const quantidadeCursos = cursos.length;
+    res.json({ cursos: cursos, qntcursos: quantidadeCursos });
     res.json("Teste2");
   } catch (error) {
     console.error(error);
