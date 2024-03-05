0186536955dfe93a4dc601d9189134d2019bc97b
evan <evandromariano49@gmail.com>
2024-03-05 14:20:19 -0300

    feat: testing 2

--- routes/users.js
+++ routes/users.js
@@ -35,7 +35,7 @@ router.get("/sub/courses/", verifyToken, userController.getSubscribedCourses);
 
 router.get("/course/:courseId/certificate", verifyToken, userController.getCertificate);
 
-router.get("/course/certificate1", verifyToken, userController.getCertificate1);
+router.get("/course/certificate1", userController.getCertificate1);
 
 module.exports = router;
 
