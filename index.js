d8f6fc2a8bdcb6ed8048a07a3028089ebb828564
evan <evandromariano49@gmail.com>
2024-03-06 16:47:52 -0300

    fix: test5

--- index.js
+++ index.js
@@ -12,10 +12,11 @@ const cookieParser = require("cookie-parser");
 app.use(express.json());
 
 app.use(cors({
-  "origin": "*",
-  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-  "preflightContinue": false,
-  "optionsSuccessStatus": 204
+  origin: ['http://localhost:4200', 'https://machine-learning-web-application-private.vercel.app'],
+  credentials: true,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204
 }));
 
 app.use(cookieParser());
